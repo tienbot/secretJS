ad3e6f0d132bf8091d4ef1c458f6234ddfcab186
Tienbot <tien.ru@mail.ru>
2025-04-25 11:02:59 +0300

    test5

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log('test4');
 
-const secret = process.env.MY_SECRET;
-console.log('Секретный ключ:', secret );
+// Этот файл будет использоваться как шаблон
+const MY_SECRET = '%%SECRET_PLACEHOLDER%%';
+
+// Остальной ваш код
+console.log('Secret value:', MY_SECRET);
