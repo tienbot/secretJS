7079a69b86c6b638617543bf274afbcb8c3b8f19
Tienbot <tien.ru@mail.ru>
2025-04-25 10:59:28 +0300

    test4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-// const secret = process.env.MY_SECRET;
-// console.log('Секретный ключ:', secret );
-console.log('test3');
+console.log('test4');
+
+const secret = process.env.MY_SECRET;
+console.log('Секретный ключ:', secret );
